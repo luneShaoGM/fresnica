9dcbaf8e0704958d0ce0a150526c16f29bf94eb4
luneShaoGM <72595256+luneShaoGM@users.noreply.github.com>
2026-08-26 11:11:38 +0800

    test: transform Stellar SDK ESM dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: '@react-native/jest-preset',
   testMatch: ['**/__tests__/**/*.test.ts'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|uint8array-extras)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@stellar/(stellar-sdk|js-xdr)|@exodus/bytes|@noble/(ed25519|hashes)|uint8array-extras|smol-toml|feaxios|eventsource)/)',
   ],
 };
