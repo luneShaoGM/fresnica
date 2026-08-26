b66bb039c675306232dcd08d899f34a5a1fe7495
luneShaoGM <72595256+luneShaoGM@users.noreply.github.com>
2026-08-26 10:38:18 +0800

    feat: align Fresnica Core port with native adapter

--- src/core/fresnica/FresnicaCore.ts
+++ src/core/fresnica/FresnicaCore.ts
@@ -1,28 +1,41 @@
 import type {
   AccountIdentity,
-  CoreCompatibility,
-  ExportedSigningMaterial,
-  ExportSigningMaterialInput,
+  ApplyEd25519SignatureInput,
+  DeriveMnemonicSignerInput,
+  Ed25519SigningRequest,
   GeneratedSigner,
   GenerateMnemonicInput,
+  PrepareEd25519SigningInput,
   ProtectedSigner,
   ProtectMnemonicInput,
   ProtectSecretInput,
+  RegisterSignerSystemAuthInput,
   ReprotectInput,
-  SignProtectedTransactionInput,
+  RevealedSigningMaterial,
+  RevealInput,
+  SignWithPasscodeInput,
+  SignWithSystemAuthInput,
 } from './types';
 
 export interface FresnicaCore {
-  getCompatibility(): Promise<CoreCompatibility>;
   parseAccount(address: string): Promise<AccountIdentity>;
-  generateMnemonic(input: GenerateMnemonicInput): Promise<GeneratedSigner>;
-  protectMnemonic(input: ProtectMnemonicInput): Promise<ProtectedSigner>;
   protectSecret(input: ProtectSecretInput): Promise<ProtectedSigner>;
-  signProtectedTransaction(
-    input: SignProtectedTransactionInput,
-  ): Promise<string>;
+  protectMnemonic(input: ProtectMnemonicInput): Promise<ProtectedSigner>;
+  generateMnemonic(input: GenerateMnemonicInput): Promise<GeneratedSigner>;
+  deriveMnemonicSigner(input: DeriveMnemonicSignerInput): Promise<ProtectedSigner>;
   reprotect(input: ReprotectInput): Promise<ProtectedSigner>;
-  exportSigningMaterial(
-    input: ExportSigningMaterialInput,
-  ): Promise<ExportedSigningMaterial>;
+  reveal(input: RevealInput): Promise<RevealedSigningMaterial>;
+  prepareEd25519Signing(
+    input: PrepareEd25519SigningInput,
+  ): Promise<Ed25519SigningRequest>;
+  applyEd25519Signature(input: ApplyEd25519SignatureInput): Promise<string>;
+  canUseSystemAuth(): Promise<boolean>;
+  hasSystemAuthDomain(): Promise<boolean>;
+  initializeSystemAuth(reason: string): Promise<boolean>;
+  registerSignerSystemAuth(input: RegisterSignerSystemAuthInput): Promise<boolean>;
+  hasSignerSystemAuth(expectedSignerPublicKey: string): Promise<boolean>;
+  removeSignerSystemAuth(expectedSignerPublicKey: string): Promise<boolean>;
+  removeSystemAuthDomain(): Promise<boolean>;
+  signWithSystemAuth(input: SignWithSystemAuthInput): Promise<string>;
+  signWithPasscode(input: SignWithPasscodeInput): Promise<string>;
 }
