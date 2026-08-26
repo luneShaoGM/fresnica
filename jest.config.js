b06fd1b2e2d5f72d4f36e929423d0df9da1c38ef
luneShaoGM <72595256+luneShaoGM@users.noreply.github.com>
2026-08-26 11:10:18 +0800

    test: transform Stellar SDK ESM helper in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|uint8array-extras)/)',
+  ],
 };
