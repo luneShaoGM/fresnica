4da170b344a539f550d7b91b8e4f5c571d41eb43
luneShaoGM <72595256+luneShaoGM@users.noreply.github.com>
2026-08-26 10:40:06 +0800

    chore: export native adapter boundary

--- src/core/fresnica/index.ts
+++ src/core/fresnica/index.ts
@@ -1,2 +1,5 @@
 export type { FresnicaCore } from './FresnicaCore';
 export * from './types';
+export * from './compatibility';
+export type { NativeFresnicaCoreModule } from './native/NativeFresnicaCoreModule';
+export { ReactNativeFresnicaCore } from './native/ReactNativeFresnicaCore';
