8b5afe81825738a97e52eebefdb34693c0d7650b
luneShaoGM <72595256+luneShaoGM@users.noreply.github.com>
2026-08-28 16:55:38 +0800

    test: isolate Realm integration Jest config

---
+++ jest.realm.config.js
@@ -0,0 +1,10 @@
+const baseConfig = require('./jest.config');
+
+module.exports = {
+  ...baseConfig,
+  testMatch: [
+    '<rootDir>/src/platform/persistence/realm/integration/**/*.integration.ts',
+  ],
+  testEnvironment: 'node',
+  watchman: false,
+};
